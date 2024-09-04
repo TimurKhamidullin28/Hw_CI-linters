a59e2109a6dc490a507aa4afac9a69ba46143161
Тимур Хамидуллин <167854616+TimurKhamidullin28@users.noreply.github.com>
2024-09-04 12:46:18 +0500

    Update database.py

--- src/database.py
+++ src/database.py
@@ -1,12 +1,13 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import async_sessionmaker
 
 DATABASE_URL = "sqlite+aiosqlite:///./cookbook.db"
 
-engine = create_async_engine(DATABASE_URL, echo=True)
-async_session = sessionmaker(
-    engine, expire_on_commit=False, class_=AsyncSession
+async_engine = create_async_engine(DATABASE_URL, echo=True)
+async_session = async_sessionmaker(
+    async_engine, expire_on_commit=False, class_=AsyncSession
 )
+
 session = async_session()
 Base = declarative_base()
